269f3a0404bd22bd21aca32960c248ec1f49354b
Evgeniy Abramov <i@kio.ninja>
2023-05-30 19:13:47 +0400

    chore: upd

--- state_sync.sh
+++ state_sync.sh
@@ -7,7 +7,7 @@ SNAP_RPC="https://m-s1-tm.haqq.sh:443"
 LATEST_HEIGHT=$(curl -s $SNAP_RPC/block | jq -r .result.block.header.height)
 
 # Calculate the height of the block to be trusted
-BLOCK_HEIGHT=$((LATEST_HEIGHT - 10000))
+BLOCK_HEIGHT=$((LATEST_HEIGHT - 1000))
 
 # Retrieve the hash of the block to be trusted
 TRUST_HASH=$(curl -s "$SNAP_RPC/block?height=$BLOCK_HEIGHT" | jq -r .result.block_id.hash)

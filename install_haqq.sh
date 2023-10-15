9aeef61faffbe44f9f896122309454e87b70c18b
Evgeniy Abramov <5331145+kioqq@users.noreply.github.com>
2023-10-15 18:49:05 +0400

    Update install_haqq.sh

--- install_haqq.sh
+++ install_haqq.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# Install HAQQ 1.4.1
+# Install HAQQ 1.6.0
 
 # Change directory to the user's home directory
 cd $HOME
 
 # Define the version of HAQQ to be installed
-HAQQ_VERSION="v1.4.1"
+HAQQ_VERSION="v1.6.0"
 
 # Clone the HAQQ repository for the specified version
 git clone -b $HAQQ_VERSION https://github.com/haqq-network/haqq
